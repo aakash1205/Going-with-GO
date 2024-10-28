fc51b55fedfd4f8ba82b85f181098a74317e2817
aakash1205 <114174835+aakash1205@users.noreply.github.com>
2024-10-28 21:54:31 +0530

    learnt about strings package and few methods

---
+++ 05conversation/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to my pizza app")
+	fmt.Println("Please rate our pizza between 1 and 5:")
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	fmt.Println("Thanks for reading ", input)
+	numrating, err := strconv.ParseFloat(strings.TrimSpace(input), 64) // strings is a package 
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Print("rating is updated as: ", numrating+1)
+	}
+
+}
